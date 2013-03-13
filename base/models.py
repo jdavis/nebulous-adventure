a3559d0669547e49eef6904bc9428bb3b9b9e572
Robbie Sheehy <gameboy1092@gmail.com>
2013-03-13 13:49:53 -0500

    Got rid of some code to start off on a clean slate so we can properly follow the development process.

--- base/models.py
+++ base/models.py
@@ -1,52 +1,13 @@
 from google.appengine.ext import db
 
-
-import logging
-
-class Item(db.Model):
-	name = db.StringProperty() 
-	description = db.StringProperty()
+class Game(Object):
+    pass
 
 class Character(db.Model):
-	name = db.StringProperty() 	
-	description = db.StringProperty()
-	items = db.ListProperty(db.Key) 
-	script = db.StringProperty()
-
-class Area(db.Model):
-	name = db.StringProperty() 	
-	description = db.StringProperty()
-	items = db.ListProperty(db.Key) 
-	characters = db.ListProperty(db.Key)
-	connecting_areas = db.ListProperty(db.Key) 
-
-	def get_direction_description(self, direction):
-		connecting_area = self.get_direction(direction)
-		if connecting_area is not None:
-			return connecting_area.description
-		return 'Nothing over there'
-
-	def get_direction(self, direction):
-		new_area_key = self.connecting_areas[direction]
-		return Actions().get_area_by_key(new_area_key)
+    pass
 
 class Player(db.Model):
-	name = db.StringProperty()
-	items = db.ListProperty(db.Key)
-	area = db.ReferenceProperty(Area)
+    pass
 
-# Datatable actions
-class Actions():
-	def create_new_player(self):
-		new_player = models.Player()
-		new_player.put()
-		return new_player
-
-	def get_player_by_key(self, key):
-		return db.get(key)
-
-	def get_area_by_key(self, key):
-		return db.get(key)	
-
-
-
+class Area(db.Model):
+    pass
