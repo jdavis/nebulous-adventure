5b8799ed9f9ed67cdc7853dab7173e3012a32c57
Robbie Sheehy <gameboy1092@gmail.com>
2013-03-15 12:06:37 -0500

    Mocking up Interfaces.

--- base/models.py
+++ base/models.py
@@ -1,13 +1,79 @@
 from google.appengine.ext import db
 
+
 class Game(Object):
-    pass
+    def look(self, uid, direction):
+        pass
+
+    def move(self, uid, direction):
+        pass
+
+    def examine(self, uid, item_name):
+        pass
+
+    def talk(self, uid, char_name):
+        pass
+
+    def eat(self, uid, item_name):
+        pass
+
 
 class Character(db.Model):
-    pass
+    def talk(self):
+        pass
+
 
 class Player(db.Model):
-    pass
+    def get_item(self, item_name):
+        pass
+
+    def get_direction(self, direction):
+        pass
+
+    def get_current_area(self):
+        pass
+
+    def set_area(self, area):
+        pass
+
+    def eat_item(self, item_name):
+        pass
+
 
 class Area(db.Model):
-    pass
+    def get_description(self):
+        pass
+
+    def get_direction(self, direction):
+        pass
+
+    def talk_to(self, char_name):
+        pass
+
+    def look(self, direction):
+        pass
+
+
+class Item(db.Model):
+    def get_description(self):
+        pass
+
+    def eat_item(self):
+        pass
+
+
+class DataStore(Object):
+    def get_item_by_name(self, item_name):
+        pass
+
+    def put_player(self, player):
+        pass
+
+    def get_player(self, id):
+        pass
+
+    def get_character_by_name(self, name):
+        pass
+
+    def get_area_by_id(self, id):
+        pass
